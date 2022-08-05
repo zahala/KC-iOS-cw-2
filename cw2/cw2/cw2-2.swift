6751595b79c8033c8d164d62e51ed42153287326
zahraa <zahraa05210@gmail.com>
2022-08-05 14:31:52 +0300

    done

--- cw2/cw2/cw2-2.swift
+++ cw2/cw2/cw2-2.swift
@@ -9,12 +9,107 @@ import SwiftUI
 
 struct cw2_2: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Image("baird")
+                .resizable()
+            VStack{
+                HStack{
+                  Image(systemName:
+                            "cube.fill")
+                    Spacer()
+                    Text("العاصمه")
+                    Spacer()
+                    Image(systemName:
+                            "gearshape.fill")
+                }.padding()
+                
+                HStack{
+                    Text("12:28")
+                        .font(.system(size:
+                            80,weight:
+                                .medium,design:
+                                .serif))
+                    Text("31")
+                        .font(.title2)
+                        .padding(.top,40)
+                }
+                
+                Text("بافي على الاذان")
+                VStack{
+                    HStack{
+                    Image(systemName:
+                            "chevron.left")
+                    Spacer()
+                    Text("٢١فبراير-٢٠رجي")
+                    Spacer()
+                    Image(systemName:
+                        "chevron.right")
+                }.padding()
+                    Divider()
+                }.background(.white.opacity(0.4))
+                
+              
+        
+            .foregroundColor(.black)
+          VStack{
+              HStack{
+          Text("٣:٣٧ص")
+                      .font(.system(size:35))
+            Spacer()
+            Text("الفجر")
+                      .font(.system(size:35))
+          }.padding()
+              Divider()
+             
+              HStack{
+            Text("٥:٠٤ص")
+                      .font(.system(size:35))
+                Spacer()
+            Text("الشروق")
+                      .font(.system(size:35))
+             
+              }.padding()
+              Divider()
+              
+              HStack{
+              Text("١١:٤٥ص")
+                      .font(.system(size: 35))
+                  Spacer()
+              Text("الظهر")
+                      .font(.system(size:35))
+              }.padding()
+              Divider()
+              
+              HStack{
+              Text("العصر")
+                      .font(.system(size:35))
+                                   Spacer()
+              Text("٣:٢١م")
+                      .font(.system(size:35))
+              }.padding()
+              Divider()
+              
+              
+              HStack{
+              Text("٦:٢٥م")
+                      .font(.system(size:35))
+                  Spacer()
+              Text("المغرب")
+                      .font(.system(size:35))
+                      
+                  
+              }.padding()
+              Divider()
+
+        }
     }
+}
+}
 }
 
 struct cw2_2_Previews: PreviewProvider {
     static var previews: some View {
         cw2_2()
+            .previewDevice("iPhone 13")
     }
 }
